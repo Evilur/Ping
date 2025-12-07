951c3cfa35227f1e5b69b7ba9f9401d9b3564dea
Andrew Shershunov <mail@evilur.ru>
2025-12-07 20:47:38 +0300

    test: Add some tests for linked list

--- test/linked_list/linked_list.cpp
+++ test/linked_list/linked_list.cpp
@@ -1,12 +1,59 @@
 #include "../core/unit_test.h"
 #include "template/linked_list.h"
 
+static void fill_list(LinkedList<int>& list);
+
+static void print_list(std::ofstream& file, const LinkedList<int>& list);
+
 void linked_list_unit_test() {
     UnitTest test("LinkedList");
 
-    test.Run("Fill", [](std::ofstream& file) {
-        LinkedList<int> list;
-        for (int i = 0; i <= 100; i++) list.Push(i);
-        for (const int n : list) file << n << std::endl;
+    test.Run("Head - Exception", [](std::ofstream& file) {
+        const LinkedList<int> list;
+        file << list.Head();
     });
+
+    test.Run("Tail - Exception", [](std::ofstream& file) {
+        const LinkedList<int> list;
+        file << list.Tail();
+    });
+
+    test.Run("Push", [](std::ofstream& file) {
+        LinkedList<int> list;
+        fill_list(list);
+        print_list(file, list);
+    });
+
+    test.Run("Pop", [](std::ofstream& file) {
+        LinkedList<int> list;
+        fill_list(list);
+        for (int i = 0; i < 8; i++) list.Pop();
+        print_list(file, list);
+    });
+
+    test.Run("Pop - Exception", [](std::ofstream& file) {
+        LinkedList<int> list;
+        list.Pop();
+    });
+
+    test.Run("TryPop", [](std::ofstream& file) {
+        LinkedList<int> list;
+        fill_list(list);
+        for (int i = 0; i < 8; i++) list.TryPop();
+        print_list(file, list);
+    });
+}
+
+static void fill_list(LinkedList<int>& list) {
+    for (int i = 1; i <= 16; i++) list.Push(i);
+}
+
+static void print_list(std::ofstream& file, const LinkedList<int>& list) {
+    /* Print all elements */
+    for (const int n : list) file << n << std::endl;
+
+    /* Print the head and the tail */
+    file << "---" << std::endl;
+    file << "Head: " << list.Head() << std::endl;
+    file << "Tail: " << list.Tail() << std::endl;
 }
