f26b7594884c69f4b080cdac3731eaa9582cba3e
Andrew Shershunov <mail@evilur.ru>
2025-12-08 17:17:55 +0300

    test: Improve HashMap unit test

--- test/hash_map/hash_map.cpp
+++ test/hash_map/hash_map.cpp
@@ -5,40 +5,44 @@
 
 void hash_map_unit_test() {
     UnitTest test("HashMap");
+    constexpr const char* const num_str[16] = {
+        "one", "two", "three", "four",
+        "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve",
+        "thirteen", "fourteen", "fifteen", "sixteen"
+    };
 
-    test.Run("Put(key, element) - 1", [](std::ofstream& file) {
-        constexpr const char* const num_str[16] = {
-            "one", "two", "three", "four",
-            "five", "six", "seven", "eight",
-            "nine", "ten", "eleven", "twelve",
-            "thirteen", "fourteen", "fifteen", "sixteen"
-        };
+    test.Run("Put(key, element) - 1", [&num_str](std::ofstream& file) {
         HashMap<const char*, int> map(4);
         for (int i = 1; i <= 16; i++) map.Put(num_str[i - 1], i);
-        for (const char* const num : num_str) file << map.Get(num) << std::endl;
+        char* const str = new char[16];
+        for (const char* const num : num_str) {
+            strcpy(str, num);
+            file << map.Get(num) << std::endl;
+        }
+        delete[] str;
     });
 
-    test.Run("Put(key, element) - 2", [](std::ofstream& file) {
-        constexpr const char* const num_str[16] = {
-            "one", "two", "three", "four",
-            "five", "six", "seven", "eight",
-            "nine", "ten", "eleven", "twelve",
-            "thirteen", "fourteen", "fifteen", "sixteen"
-        };
+    test.Run("Put(key, element) - 2", [&num_str](std::ofstream& file) {
         HashMap<const char*, int> map(8);
         for (int i = 1; i <= 16; i++) map.Put(num_str[i - 1], i);
-        for (const char* const num : num_str) file << map.Get(num) << std::endl;
+        char* const str = new char[16];
+        for (const char* const num : num_str) {
+            strcpy(str, num);
+            file << map.Get(num) << std::endl;
+        }
+        delete[] str;
     });
 
-    test.Run("Put(key, element) - 3", [](std::ofstream& file) {
-        constexpr const char* const num_str[16] = {
-            "one", "two", "three", "four",
-            "five", "six", "seven", "eight",
-            "nine", "ten", "eleven", "twelve",
-            "thirteen", "fourteen", "fifteen", "sixteen"
-        };
+    test.Run("Put(key, element) - 3", [&num_str](std::ofstream& file) {
         HashMap<const char*, int> map(16);
         for (int i = 1; i <= 16; i++) map.Put(num_str[i - 1], i);
-        for (const char* const num : num_str) file << map.Get(num) << std::endl;
+        char* const str = new char[16];
+        for (const char* const num : num_str) {
+            strcpy(str, num);
+            file << map.Get(num) << std::endl;
+        }
+        delete[] str;
+
     });
 }
