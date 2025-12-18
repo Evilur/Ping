53ac3aea88f04b051111fa94de8c154ed9edeb12
Andrew Shershunov <mail@evilur.ru>
2025-12-18 17:12:50 +0300

    feat: Add en_US translation file

---
+++ assets/locale/en_US.ts
@@ -0,0 +1,13 @@
+<?xml version='1.0' encoding='utf-8'?>
+<TS version='2.1' language='en_US'>
+    <context>
+        <name>ChatList</name>
+        <message numerus='yes'>
+            <source>%n chat member(s)</source>
+            <translation>
+                <numerusform>%n chat member</numerusform>
+                <numerusform>%n chat members</numerusform>
+            </translation>
+        </message>
+    </context>
+</TS>
