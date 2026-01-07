f111eb584161c1c5d081bfc765d1cea4a3dd2303
Andrew Shershunov <mail@evilur.ru>
2026-01-07 18:11:30 +0300

    test: Fix dictionary tests

--- test/dictionary/dictionary.cpp
+++ test/dictionary/dictionary.cpp
@@ -5,7 +5,8 @@
 
 void dictionary_unit_test() {
     UnitTest test("Dictionary");
-    constexpr const char* const num_str[16] = {
+    constexpr int num_str_size = 16;
+    constexpr const char* const num_str[] = {
         "one", "two", "three", "four",
         "five", "six", "seven", "eight",
         "nine", "ten", "eleven", "twelve",
@@ -13,56 +14,56 @@ void dictionary_unit_test() {
     };
 
     test.Run("Put(key, element) - 1", [num_str] {
-        Dictionary<const char*, int> dict(4);
-        for (int i = 1; i <= 16; i++) dict.Put(num_str[i - 1], i);
-        char str[16];
+        Dictionary<const char*, int> dict(num_str_size / 4);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(num_str[i - 1], i);
+        char str[num_str_size];
         for (const char* const num : num_str) {
             strcpy(str, num);
-            std::cout << dict.Get(num) << std::endl;
+            std::cout << dict.Get(str) << '\n';
         }
     });
 
     test.Run("Put(key, element) - 2", [num_str] {
-        Dictionary<const char*, int> dict(8);
-        for (int i = 1; i <= 16; i++) dict.Put(num_str[i - 1], i);
-        char str[16];
+        Dictionary<const char*, int> dict(num_str_size / 2);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(num_str[i - 1], i);
+        char str[num_str_size];
         for (const char* const num : num_str) {
             strcpy(str, num);
-            std::cout << dict.Get(num) << std::endl;
+            std::cout << dict.Get(str) << '\n';
         }
     });
 
     test.Run("Put(key, element) - 3", [num_str] {
-        Dictionary<const char*, int> dict(16);
-        for (int i = 1; i <= 16; i++) dict.Put(num_str[i - 1], i);
-        char str[16];
+        Dictionary<const char*, int> dict(num_str_size);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(num_str[i - 1], i);
+        char str[num_str_size];
         for (const char* const num : num_str) {
             strcpy(str, num);
-            std::cout << dict.Get(num) << std::endl;
+            std::cout << dict.Get(str) << '\n';
         }
     });
 
     test.Run("Put(key, element) - 4", [num_str] {
-        Dictionary<int, const char*> dict(4);
-        for (int i = 1; i <= 16; i++) dict.Put(i, num_str[i - 1]);
-        for (int i = 1; i <= 16; i++) {
-            std::cout << dict.Get(i) << std::endl;
+        Dictionary<int, const char*> dict(num_str_size / 4);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(i, num_str[i - 1]);
+        for (int i = 1; i <= num_str_size; i++) {
+            std::cout << dict.Get(i) << '\n';
         }
     });
 
     test.Run("Put(key, element) - 5", [num_str] {
-        Dictionary<int, const char*> dict(8);
-        for (int i = 1; i <= 16; i++) dict.Put(i, num_str[i - 1]);
-        for (int i = 1; i <= 16; i++) {
-            std::cout << dict.Get(i) << std::endl;
+        Dictionary<int, const char*> dict(num_str_size / 2);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(i, num_str[i - 1]);
+        for (int i = 1; i <= num_str_size; i++) {
+            std::cout << dict.Get(i) << '\n';
         }
     });
 
     test.Run("Put(key, element) - 6", [num_str] {
-        Dictionary<int, const char*> dict(16);
-        for (int i = 1; i <= 16; i++) dict.Put(i, num_str[i - 1]);
-        for (int i = 1; i <= 16; i++) {
-            std::cout << dict.Get(i) << std::endl;
+        Dictionary<int, const char*> dict(num_str_size);
+        for (int i = 1; i <= num_str_size; i++) dict.Put(i, num_str[i - 1]);
+        for (int i = 1; i <= num_str_size; i++) {
+            std::cout << dict.Get(i) << '\n';
         }
     });
 }
